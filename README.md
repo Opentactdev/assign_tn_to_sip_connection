74d0e4ee0c2e075a2bca773a6c924b84c28b9be0
Alexey Matvienko <alexey.matvienko@gmail.com>
2021-03-19 18:59:37 +0600

    readme update

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ This script help Opentact User to associate all their TN to a SIP Connection
 ## Preparation
 1) [Create sip connection](https://api.opentact.org/swagger/?urls.primaryName=TAG%3A%20SIP_Connection#/SIP%20Connection/CreateSIPConnection)
 2) [Create bearer token](https://api.opentact.org/swagger/?urls.primaryName=TAG%3A%20Auth#/Auth/CreateToken)
+3) `jq` tool must be installed:
+   * MacOS: `brew install jq`
+   * Linux
+      * `apt install jq`
+      * `yum add jq`
 
 ## Execution
  ```shell
 ./assign_opentact_tn_to_sip_connection.sh "BEARER TOKEN" "SIP CONNECTION UUID"
 ```
+
+### Hints
+* if all numbers already assigned to provided sip connection, u will see message: `0 non-related numbers found`
--- assign_opentact_tn_to_sip_connection.sh
+++ assign_opentact_tn_to_sip_connection.sh
@@ -5,18 +5,19 @@ SIP_CONN_UUID=$2
 TAKE=100
 SKIP=0
 
-if [[ "x$SIP_CONN_UUID" == "x" ]]
-then
+if [[ "x$SIP_CONN_UUID" == "x" ]]; then
   echo "Usage: $0 <BEARER TOKEN> <SIP CONNECTION UUID>"
   exit 1
 fi
 
 function getNext() {
-  echo $(curl -X POST "https://api.opentact.org/rest/lease/tn/search?bearer=${BEARER}&take=$1&skip=$2" 2>/dev/null | jq '.payload')
+  res=$(curl -X POST "https://api.opentact.org/rest/lease/tn/search?bearer=${BEARER}&take=$1&skip=$2" -H  "Content-Type: application/json" -d "{\"sip_connections_skip\":[\"${SIP_CONN_UUID}\"]}" 2>/dev/null)
+  echo "${res}" | jq '.payload'
 }
 
 function getNextData() {
-  echo $(getNext $1 $2) | jq '.data'
+  next=$(getNext $1 $2)
+  echo "${next}" | jq '.data'
 }
 
 function assign() {
@@ -24,16 +25,22 @@ function assign() {
   REQ="{\"add\":["
   REQ="${REQ}"$(echo ${UUIDS} | sed -e 's/" "/","/g')
   REQ="${REQ}]}"
-  RES=$(curl -X POST "https://api.opentact.org/rest/sip/connection/${SIP_CONN_UUID}/tnlease?bearer=${BEARER}" -H "Content-Type: application/json" -d "${REQ}" 2>/dev/null)
-  echo " $(echo "${RES}" | jq '.success')"
+  curl -X POST "https://api.opentact.org/rest/sip/connection/${SIP_CONN_UUID}/tnlease?bearer=${BEARER}" -H "Content-Type: application/json" -d "${REQ}" 2>/dev/null
 }
 
 DATA=$(getNext $TAKE $SKIP)
 TOTAL=$(echo "${DATA}" | jq '.total')
+echo "${TOTAL} non-related numbers found"
 DATA=$(echo "${DATA}" | jq '.data')
 while [ "$(echo "${DATA}" | jq length)" -ne 0 ]; do
-  echo -n $SKIP" - "$((${SKIP} + ${TAKE}))" / ${TOTAL} ..."
-  assign "${DATA}"
+  echo -n $SKIP" - "$((${SKIP} + ${TAKE}))" / ${TOTAL} ... "
+  RES=$(assign "${DATA}")
+
+  if [[ $(echo "${RES}" | jq '.success') == "true" ]]; then
+    echo "success"
+  else
+    echo "FAILED: $(echo "${RES}" | jq '.message')"
+  fi
 
   SKIP=$((${SKIP} + ${TAKE}))
   DATA=$(getNextData $TAKE $SKIP)
